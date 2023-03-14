420bb19d6a8a7d44e5ab775715f9ef2a45140a27
Rober <rp_rober@hotmail.com>
2023-03-14 12:50:51 +0100

    incluyo db.json

--- utils/adStatusImagen.js
+++ utils/adStatusImagen.js
@@ -1,10 +1,4 @@
 export function adStatusImage(ad) {
-  if (ad.state == true) {
-    ad.state = "En venta";
-
-  } else {
-    ad.state = "Compro";
-  }
 
   if (ad.img == "") {
     ad.img = "https://www.redeszone.net/app/uploads-redeszone.net/2023/02/listado_errores_http_destacada.jpg";
